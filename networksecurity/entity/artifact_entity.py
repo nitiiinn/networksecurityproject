e15b3b4b8b797420ed1fa8209a99ba11df009214
nitin0070 <nitin21kothiyal@gmail.com>
2025-10-09 23:03:28 +0530

    model

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -25,3 +25,9 @@ class ClassificationMetricArtifact:
     f1_score: float
     precision_score: float
     recall_score: float
+    
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
